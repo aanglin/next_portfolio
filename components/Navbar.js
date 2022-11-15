572755f3349d427963be76996c600a375e4ce082
Aaron Anglin <aaron.anglin101@gmail.com>
2022-11-15 16:23:46 -0600

    finished navbar and main

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
@@ -6,6 +6,11 @@ import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import {BsFillPersonLinesFill} from "react-icons/bs"
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false)
+
+    const handleNav = () => {
+        setNav(!nav)
+    }
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100]">
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
@@ -32,13 +37,14 @@ const Navbar = () => {
               </li>
             </Link>
           </ul>
-          <div className="md:hidden">
+          <div onClick={handleNav} className="md:hidden">
             <AiOutlineMenu size={25} />
           </div>
         </div>
       </div>
-      <div className="fixed left-0 top-0 w-full h-screen bg-black/70">
-        <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500 ">
+      <div className={nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/90" : ''}>
+        <div className={nav ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500 " : 
+        "fixed left-[-100%] top-0 p-10 ease-in duration-500 "}>
           <div>
             <div className="flex w-full items-center justify-between">
               <Image
@@ -47,8 +53,8 @@ const Navbar = () => {
                 height="35"
                 alt="/"
               />
-              <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
-                <AiOutlineClose size={25} />
+              <div onClick={handleNav}className=" rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
+                <AiOutlineClose  />
               </div>
             </div>
             <div className="border-b border-gray-300 my-4">
