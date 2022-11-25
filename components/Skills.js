29e1b7170e0ee330cf703bac639f3ac365971af6
Aaron Anglin <aaron.anglin101@gmail.com>
2022-11-25 09:52:49 -0600

    added bootstrap skill

--- components/Skills.js
+++ components/Skills.js
@@ -100,6 +100,21 @@ const Skills = () => {
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <Image
+                  src="https://getbootstrap.com/docs/5.2/assets/brand/bootstrap-logo-shadow.png"
+                  width="64"
+                  height="64"
+                  alt="/"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Bootstrap</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
                   src={mongoDB}
                   width="64"
                   height="64"
