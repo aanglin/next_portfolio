ad290a1c49439c3f9823323717a7f8535d3d346f
Aaron Anglin <aaron.anglin101@gmail.com>
2022-11-26 16:14:28 -0600

    added new project to page

--- components/Projects.js
+++ components/Projects.js
@@ -5,22 +5,47 @@ import Weather from "../public/assets/projects/Weather.png";
 import FactorFiction from "../public/assets/projects/FactorFiction.png";
 import Notes from "../public/assets/projects/Notes.png";
 import TechBlog from "../public/assets/projects/techBlog.png";
-
+import Hulu from "../public/assets/projects/Hulu2.0.png";
 
 const Projects = () => {
   return (
-    <div id='projects' className="w-full">
+    <div id="projects" className="w-full">
       <div className="max-w-[1240px] mx-auto px-2 py-16">
         <p className="text-xl tracking-widest uppercase text-[#5651e5]">
           Projects
         </p>
         <h2 className="py-4">What I have built</h2>
         <div className="grid md:grid-cols-2 gap-8">
- <ProjectItems title='Recipe App' bgImage={Recipe} projectUrl='/recipe'/>
- <ProjectItems title='Weather App' bgImage={Weather} projectUrl='/weather'/>
- <ProjectItems title='Fact-or-Fiction App' bgImage={FactorFiction} projectUrl='/factorfiction'/>
- <ProjectItems title='Notes App' bgImage={Notes} projectUrl='/notes'/>
- <ProjectItems title='TechBlog App' bgImage={TechBlog} projectUrl='/techBlog'/>
+          <ProjectItems 
+          title="Recipe App" 
+          bgImage={Hulu} 
+          projectUrl="/hulu" 
+          />
+          <ProjectItems
+            title="Recipe App"
+            bgImage={Recipe}
+            projectUrl="/recipe"
+          />
+          <ProjectItems
+            title="Weather App"
+            bgImage={Weather}
+            projectUrl="/weather"
+          />
+          <ProjectItems
+            title="Fact-or-Fiction App"
+            bgImage={FactorFiction}
+            projectUrl="/factorfiction"
+          />
+          <ProjectItems 
+          title="Notes App" 
+          bgImage={Notes} 
+          projectUrl="/notes" 
+          />
+          <ProjectItems
+            title="TechBlog App"
+            bgImage={TechBlog}
+            projectUrl="/techBlog"
+          />
         </div>
       </div>
     </div>
