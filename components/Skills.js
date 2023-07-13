64f692a44ef9a291e514f9a12dc74f0899c77ed8
Aaron Anglin <aaron.anglin101@gmail.com>
2023-07-13 12:02:49 -0500

    added typescript skills

--- components/Skills.js
+++ components/Skills.js
@@ -10,6 +10,7 @@ import Node from "../public/assets/skills/node.png";
 import Express from "../public/assets/skills/javascript.png";
 import nextJS from "../public/assets/skills/nextjs.png";
 import Github from "../public/assets/skills/github1.png";
+import typescript from "../public/assets/skills/typescript.png";
 
 const Skills = () => {
   return (
@@ -47,6 +48,16 @@ const Skills = () => {
               </div>
               <div className="flex flex-col items-center justify-center">
                 <h3>JavaScript</h3>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 shadow-xl shadow-gray-400 rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image src={typescript} width="64" height="64" alt="/" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Typescript</h3>
               </div>
             </div>
           </div>
